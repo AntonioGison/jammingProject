a74476bd8feb74ee29016093cbed71474f55d800
Antonio Gison <antonio@gison.it>
2023-06-19 18:43:26 +0100

    front end first component added

--- jamming/src/Components/App/App.js
+++ jamming/src/Components/App/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-// import './Components/SearchBar/SearchBar.js';
+import SearchBar from '../SearchBar/SearchBar';
 
 class App extends React.Component{
   render(){
@@ -8,7 +8,7 @@ class App extends React.Component{
       <div>
       <h1>Ja<span className="highlight">mmm</span>ing</h1>
       <div className="App">
-      {/* <SearchBar />  */}
+       <SearchBar />  
         <div className="App-playlist">
          {/* <SearchResults /> */}
           {/* <Playlist /> */}
