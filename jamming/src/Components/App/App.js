50706a4c2b625468438946ec34fa73550b9c5269
Antonio Gison <antonio@gison.it>
2023-06-20 14:12:30 +0100

    tracklist added with hardcode samples

--- jamming/src/Components/App/App.js
+++ jamming/src/Components/App/App.js
@@ -5,6 +5,17 @@ import SearchResults from '../SearcResults/SearchResults';
 import Playlist from '../Playlist/Playlist';
 
 class App extends React.Component{
+  constructor(props) {
+    super(props);
+
+    this.state = {
+            searchResults : [{name: 'pippo', artist: 'Giggione', album: 'a me m piac a nutella', id: '90435'},
+                            {name: 'john', artist: 'bow', album: 'not water', id: '34345'},
+                            {name: 'bell', artist: 'Madonna', album: 'best album ever', id: '2445'}]
+    };
+  }
+
+
   render(){
     return (
       <div>
@@ -12,7 +23,7 @@ class App extends React.Component{
       <div className="App">
        <SearchBar />  
         <div className="App-playlist">
-         <SearchResults />
+         <SearchResults searchResults={this.state.searchResults}/>
           <Playlist /> 
         </div>
       </div>
--- jamming/src/Components/TrackList/TrackList.js
+++ jamming/src/Components/TrackList/TrackList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './TrackList.css';
+import Track from '../Track/Track';
 
 
 class TrackList extends React.Component{
@@ -7,7 +8,12 @@ class TrackList extends React.Component{
     render(){
         return(
             <div className="TrackList">
-    {/* <!-- You will add a map method that renders a set of Track components  --> */}
+        {
+        this.props.tracks.map(track => {
+            return <Track track={track}
+                 key={track.id} />
+        })
+        }
     </div>
         )
     }
